bd900cb04984ce6807539da695afdbfc52f9c5e0
Gyromitte <gyromitte@gmail.com>
2025-03-31 19:12:02 -0600

    changes

--- react-sakilla/src/admin/InventoryPage.js
+++ react-sakilla/src/admin/InventoryPage.js
@@ -3,3 +3,15 @@ import axios from "axios";
 import "bootstrap/dist/css/boostrap.min.csss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import ReusableTable from "../ReusableTable";
+
+const InventoryPage = () => {
+    const [inventory, setInventory] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [lastPage, setLastPage] = useState(1);
+
+    const loadInventory = async (page) => {
+        try {
+            const response = await axios.get()
+        }
+    }
+}
