2ecd678ef14d35648d46c612b0f195de46431326
EdwardLynx24 <eduardodarakr24@gmail.com>
2025-03-31 22:45:08 -0600

    Login component

---
+++ react-sakilla/src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./admin/navbar";
+
+function Login() {
+    return (
+        <form>
+            
+        </form>
+    );
+}
+
+export default Login;
