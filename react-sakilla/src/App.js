2c481719eacfb91c9f832bb756f3c443547057be
EdwardLynx24 <eduardodarakr24@gmail.com>
2025-03-31 22:47:54 -0600

    Login

--- react-sakilla/src/App.js
+++ react-sakilla/src/App.js
@@ -4,11 +4,13 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import InventoryPage from "./admin/InventoryPage";
 import 'bootstrap';
+import Login from "./Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/InventoryPage" element={<InventoryPage />} />
       </Routes>
     </BrowserRouter>
