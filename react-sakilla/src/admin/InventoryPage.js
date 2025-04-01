6b1e1338f8ebf806a727d670858d5b04456aa03d
Gyromitte <gyromitte@gmail.com>
2025-03-31 19:11:50 -0600

    changes

---
+++ react-sakilla/src/admin/InventoryPage.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/boostrap.min.csss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import ReusableTable from "../ReusableTable";
